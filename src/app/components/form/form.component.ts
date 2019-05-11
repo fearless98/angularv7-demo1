1529421fb0b068e06da6abdfcfb04637ba2e93a3
方家宜 <fearless98@aliyum.com>
2019-05-11 19:30:00 +0800

    form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public peopleInfo: any = {
+    username : '',
+    gender: '2',
+    cityList:[ "成都", "北京", "上海","深圳","广州"],
+    city: "成都",
+    hobby:[{
+      title: 'eat',
+      checked: false
+    },{
+        title: 'sleep',
+        checked: false
+    },{
+      title: 'code',
+      checked: true
+    }],
+    mark:''
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
+  doSubmit(){
+    //方法一：jquery dom操作
+    // let usernameDom : any = document.getElementById("username");
+    // console.log(usernameDom.value);
+
+    //方法二：双向数据绑定
+  // console.log(this.peopleInfo.gender);
+  // console.log(this.peopleInfo.city);
+  console.log(this.peopleInfo);
+  
+  }
 }
+
