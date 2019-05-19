efe0263c2229cefcaeaa7e8406542fd2ef4b8fc2
方家宜 <fearless98@aliyum.com>
2019-05-19 15:54:43 +0800

    doDoList

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -7,8 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
 
+  public keyWord: string;
+  public todoList: any[] = [];
   constructor() { }
 
+  doAdd(e) {
+      if (e.keyCode == 13) {
+        if (!this.checkKeywordExist(this.todoList, this.keyWord)) {
+        this.todoList.push({
+          title: this.keyWord,
+          status: 0  //0表示待办事项 1表示已完成事项
+        });
+        this.keyWord = '';
+        }
+        else{
+          alert(this.keyWord + "已存在");
+          this.keyWord = '';
+        }
+      }
+  }
+  doDelete(key){
+   this.todoList.splice(key, 1);
+  }
+  checkKeywordExist(todoList: any, keyWord: any){
+    if(!keyWord) return false;
+    for ( let i = 0; i  < this.todoList.length ; i++) {
+      if (todoList[i].title==keyWord) {
+        return true;
+      }
+    }
+    return false;
+  }
   ngOnInit() {
   }
 
